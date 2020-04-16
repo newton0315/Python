3e42bc47a9c8843eb4dbe1011f62fd51fb47c9ee
Newton Lee <newton.lee@samsung.com>
2020-04-17 06:49:11 +0900

    Multithreading to update the mssage

--- simple_client/motd/daemon.py
+++ simple_client/motd/daemon.py
@@ -3,8 +3,16 @@
 import http.server
 import socketserver
 
+import threading
+from motd import make_page
+
 handler = http.server.SimpleHTTPRequestHandler
 
+def update_motd():
+	motd.make_page('data.txt')
+
 with socketserver.TCPServer(('', 10001), handler) as httpd:
+	threading.Timer(1800.0, update_motd).start()
 	print('Server listening on port 10001...')
 	httpd.serve_forever()
+
